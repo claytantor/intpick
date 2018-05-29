a772fa569a7bb0625829c321a864e0e0d904f3f6
claytantor <claytantor@gmail.com>
2018-05-28 23:14:11 -0700

    seems to be working ok needs more edge tests

--- src/main/java/org/claytantor/controller/IntpickerController.java
+++ src/main/java/org/claytantor/controller/IntpickerController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.claytantor.model.IntpickerModel;
 import org.claytantor.service.IntegerPickerService;
+import org.claytantor.service.ServiceExcpetion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -25,25 +28,30 @@ public class IntpickerController {
 
 
     @Autowired
-    private IntegerPickerService hashMakerService;
+    private IntegerPickerService integerPickerService;
 
 
     @ApiOperation(value = "Find integers that equal sum",
             notes = "Retrieving the model for the sum of integers.", response = IntpickerModel.class)
     @ApiResponses({
-            @ApiResponse(code = 200, message = "Success", response = IntpickerModel.class)
+            @ApiResponse(code = 200, message = "Success", response = IntpickerModel.class),
+            @ApiResponse(code = 406, message = "Not Acceptable", response = Map.class)
     })
     @RequestMapping(method = RequestMethod.POST, produces = "application/json", value="/api/intpicker")
     public ResponseEntity<?> items(@RequestBody IntpickerModel request) {
 
         logger.debug("IntpickerController.items.POST called.");
 
+        try {
+            return new ResponseEntity<IntpickerModel>(
+                    integerPickerService.pickIntegers(
+                            request.getItems(), request.getCount(), request.getSum()), HttpStatus.OK);
+        } catch (ServiceExcpetion serviceExcpetion) {
+            logger.info("there was a BL error attempting to count the integers.");
+            Map<String, String> errorModel = new HashMap<>();
+            errorModel.put("message",serviceExcpetion.getMessage());
+            return new ResponseEntity<Map<String,String>>(errorModel, HttpStatus.NOT_ACCEPTABLE);
 
-        Integer[] intArray = { 2, 4 };
-        IntpickerModel response = new IntpickerModel(Arrays.asList(intArray), request.getCount(), request.getSum());
-
-
-
-        return new ResponseEntity<IntpickerModel>(response, HttpStatus.OK);
+        }
     }
 }
